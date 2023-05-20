7743dafdce6f710f1733a8bd11e4efabd2192116
ZeckeyBay <zfatihturan@gmail.com>
2023-05-20 18:55:10 +0300

    buttons work sidebar added

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-
 let tablinks = document.getElementsByClassName("tab-links");
 let tabcontents = document.getElementsByClassName("tab-contents");
 
-let opentab = (tabname) =>{
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link")
+let opentab = (tabname) => {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+};
+
+// Smooth scrolling animation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+});
+  
+
+window.addEventListener('scroll', function() {
+    const aboutSection = document.getElementById('about');
+    const sidebar = document.getElementById('sidebar');
+    const aboutOffset = aboutSection.offsetTop;
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  
+    if (scrollPosition >= aboutOffset - 500) {
+      sidebar.classList.add('sidebar-show');
+    } else {
+      sidebar.classList.remove('sidebar-show');
     }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab")
-    }
-    event.currentTarget.classList.add("active-link")
-    document.getElementById(tabname).classList.add("active-tab")
-}
+  });
+  
+  
+  
