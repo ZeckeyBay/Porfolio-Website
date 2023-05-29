b1edf1fcbe44ec198bb1afc7cd8e47d17faa227f
ZeckeyBay <zfatihturan@gmail.com>
2023-05-29 13:49:23 +0300

    sidebar button

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
   
+let isScroll = true;
 window.addEventListener('scroll', function() {
     const aboutSection = document.getElementById('about');
     const sidebar = document.getElementById('sidebar');
     const aboutOffset = aboutSection.offsetTop;
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  
-    if (scrollPosition >= aboutOffset - 500) {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0; 
+    if ((scrollPosition >= aboutOffset - 500) && (isScroll == true)) {
       sidebar.classList.add('sidebar-show');
     } else {
       sidebar.classList.remove('sidebar-show');
@@ -38,15 +38,34 @@ window.addEventListener('scroll', function() {
   });
 
   // Sidemenu
-
   let sideMenu = document.getElementById("sidemenu")
-  
   let openmenu = () =>{
     sideMenu.style.right = "0"
   }
   let closemenu = () =>{
     sideMenu.style.right = "-200px"
   }
+  
+  const sidebarButton = document.getElementById("sidebar-btns");
+  const sidebar = document.querySelector("#sidebar");
+
+sidebarButton.addEventListener("click", function () {
+  sidebar.classList.toggle("sidebar-show");
+  isScroll = false;
+  loadSidebar();
+});
+// replace sidebar with a icon when clicked(does not work now)
+let loadSidebar = () =>{
+  if (sidebar.classList.contains("sidebar-show")) {
+    sidebarButton.classList.replace("fa-solid fa-arrow-right-from-bracket","fa-solid fa-arrow-left");
+  } else {
+    sidebarButton.classList.replace("fa-solid fa-arrow-left","fa-solid fa-arrow-right-from-bracket");
+  }
+}
+
+
+  
+
   
   
   
